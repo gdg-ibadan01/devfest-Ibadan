428bd5435083c054d8ef5b016fcbf97ecc2f69cf
Orim Dominic <orimdominic@gmail.com>
2026-08-20 05:39:21 +0100

    feat: get ticket by slug (#103)

--- backend/src/modules/ticket/ticket.service.ts
+++ backend/src/modules/ticket/ticket.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import {
   CreateTicketResponseDto,
+  GetTicketBySlugResponseDto,
   GetTicketResponseDto,
   TicketQueryDto,
 } from './dto/ticket.dto';
@@ -229,6 +230,35 @@ export class TicketsService {
 
   findByTicketNumber(ticketNumber: string) {
     return null;
+  }
+
+  async findBySlug(slug: string): Promise<GetTicketBySlugResponseDto> {
+    const ticket = await this.prisma.ticket.findUnique({
+      where: { slug },
+      select: {
+        name: true,
+        description: true,
+        price: true,
+        discount: true,
+        eventDates: true,
+        validityDates: true,
+        slug: true,
+      },
+    });
+
+    if (!ticket) {
+      throw new NotFoundException('Ticket not found');
+    }
+
+    return {
+      name: ticket.name,
+      description: ticket.description,
+      price: ticket.price.toNumber(),
+      discount: ticket.discount.toNumber(),
+      eventDates: ticket.eventDates,
+      validityDates: ticket.validityDates,
+      slug: ticket.slug,
+    };
   }
 
   verifyTicket(ticketNumber: string) {
